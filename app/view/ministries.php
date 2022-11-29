8bb3bb0349785579cebc6a4dd8522ee8f22d6c13
estherLove22 <91344188+estherLove22@users.noreply.github.com>
2022-11-28 19:09:01 -0500

    Create Ministry

---
+++ app/view/ministries.php
@@ -0,0 +1,48 @@
+<div id="content">
+		<div id="publication">
+		<div class="row flex">
+
+			<div class="text-holder">
+				<h4>MINISTRIES</h4>
+				<h1>PUBLICATION</h1>
+				<p>The Metropolitan Bible Baptist Church Toronto is an independent church holding fast in the fundamental truths of the King James Version of the Bible. We are a Bible-believing, Soul-winning, family-oriented and Christ-centered church. We are committed to spreading the good news of salvation through faith in the Lord Jesus Christ.</p>
+
+				<p>We are located in the North York area of the city of Toronto. We are a Filipino congregation reaching out to everybody regardless of culture or nationality. For counseling and advice, and to know more about our church and ministries, please call <a href="tel:<?php $this->info("phone"); ?>"><?php $this->info("phone"); ?></a> or email us at <a href="mailto:<?php $this->info("email"); ?>"><?php $this->info("email"); ?></a> and ask for Pastor Gamy Romano. We welcome you to our church website and we hope that you will find all the information you need.</p>
+
+				<a class="buttons" href="about#content">Learn More</a>
+			</div>
+			<div class="img-holder">
+				<img src="public/images/content/welcome.jpg" align="welcome image">
+			</div>
+		</div>
+		<div id="choir">
+			<div class="row flex">
+				<div class="img-holder">
+				<img src="public/images/content/welcome.jpg" align="welcome image">
+			</div>
+		
+			<div class="text-holder">
+				<h4>MINISTRIES</h4>
+				<h1>FINANCE</h1>
+				<p>I praise and Thank God for giving me
+    <br/>a Church - Metropolitan Bible
+    <br/>Baptist Church Toronto.
+    <br/> I'm blessed and privileged to
+    <br/> witness the growth of our
+    <br/>Church - from its humble beginning
+    <br/> to where it is now. MBBC is a
+    <br/>living testament of God's grace - His
+    <br/>provision is all sufficient. Through
+    <br/> the years the giving of the Church
+    <br/>increases and always been above the
+    <br/> year's budget. I commend the faithful
+    <br/>members of this Church who have learned and
+    <br/> applied the Bible principles on giving...
+    <br/>Malachi 3:10". --- Sis. Ofel Espedido</p>
+
+				<a class="buttons" href="about#content">Learn More</a>
+			</div>
+			
+			</div>
+		</div>
+	</div>
