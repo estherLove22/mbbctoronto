3924c8b0d3fdd89ea94de9577117a9ad7948124b
estherLove22 <91344188+estherLove22@users.noreply.github.com>
2022-11-29 20:37:32 -0500

    update ministry
    composition , publication, finance

--- app/view/ministries.php
+++ app/view/ministries.php
@@ -1,30 +1,93 @@
-	<div id="content">
-		<div id="publication">
+<div id="content">
+	<br/>
+	<div id="composition">
 		<div class="row flex">
+		<div class="img-holder">
+				<img src="public/images/content/composition.jpg" width="627" height="418" />
+			</div>
 
 			<div class="text-holder">
-				<h4>MINISTRIES</h4>
-				<h1>PUBLICATION</h1>
-				<p>The Metropolitan Bible Baptist Church Toronto is an independent church holding fast in the fundamental truths of the King James Version of the Bible. We are a Bible-believing, Soul-winning, family-oriented and Christ-centered church. We are committed to spreading the good news of salvation through faith in the Lord Jesus Christ.</p>
 
-				<p>We are located in the North York area of the city of Toronto. We are a Filipino congregation reaching out to everybody regardless of culture or nationality. For counseling and advice, and to know more about our church and ministries, please call <a href="tel:<?php $this->info("phone"); ?>"><?php $this->info("phone"); ?></a> or email us at <a href="mailto:<?php $this->info("email"); ?>"><?php $this->info("email"); ?></a> and ask for Pastor Gamy Romano. We welcome you to our church website and we hope that you will find all the information you need.</p>
+				<h1>COMPOSITION</h1>
+				<h4>MINISTRIES</h4>
+				
+				<p>
+I truly praise the Lord for His bountiful blessings
+for MBBC, and that includes enabling us to start
+a new ministry of composing songs.
+It all started with a desire in doing more
+for the glory of the Lord. During
+the pandemic in the year 2020, we were
+inspired and challenged to still continue serving and giving our best for God.
+Many of our members and even our beloved
+Pastor, by God's grace, were able to
+write or create God-honouring sacred songs. Now, we have made more than a hundred
+compositions in the past couple years.
+It has been a great and wonderful
+blessing and encouragement, not only
+to MBBC Toronto, but to many Christians
+around the world. All the glory and
+praise belongs to our loving God alone. - Chelzy Joy Respicio
+</p>
 
 				<a class="buttons" href="about#content">Learn More</a>
 			</div>
+			
+		</div>
+	</div>
+		<br/>
+
+		<div id="publication">
+			<div class="row flex">
+				
+			<div class="text-holder">
+				
+				<h1>PUBLICATION</h1>
+				<h4>MINISTRIES</h4>
+				<p>Looking back, I remember a time when the publications
+  				<br/>  ministry only composed of a few people. Looking
+ 				<br/>   now, it's amazing to see how God has added
+				<br/>   to this ministry, not only in terms  of what we 
+				<br/> are able to do but also in the people He uses.
+				<br/>   I praise and thank God for giving every one of
+   				<br/>  us the privilege to 
+be part of this wonderful ministry. Through this ministry,
+ we are able to witness God's faithfulness time and time
+ again through answered prayers, finance and attendance 
+reports. It is also through this ministry that we are able
+ to help other ministries. I thank God for giving us 
+wisdom, strength, and grace to finish our projects, especially
+ during late nights. I also thank Him for a church family that
+ is always willing to help whenever it is needed. Personally,
+ this ministry has a special place in my heart because I've 
+been part of this ministry since I was fourteen. I look forward
+ to God using not only me, but all the wonderful people in 
+this ministry, for many more years to come. I cannot wait to 
+see what God has in store for us!
+-Trixie Velasco
+</p>
+<br/>
+				<a class="buttons" href="about#content">Learn More</a>
+			</div>
+
 			<div class="img-holder">
-				<img src="public/images/content/welcome.jpg" align="welcome image">
+				<img src="public/images/content/publications.jpg" width="627" height="418" />
+			</div>
+			
 			</div>
 		</div>
-		<div id="choir">
-			<div class="row flex">
-				<div class="img-holder">
-				<img src="public/images/content/welcome.jpg" align="welcome image">
+		<br/>
+	
+	<div id="finance">
+		<div class="row flex">
+			<div class="img-holder">
+				<img src="public/images/content/finance.jpg"width="627" height="418" />
 			</div>
-		
 			<div class="text-holder">
-				<h4>MINISTRIES</h4>
+				
 				<h1>FINANCE</h1>
-				<p>I praise and Thank God for giving me
+				<h4>MINISTRIES</h4>
+	<p>I praise and Thank God for giving me
     <br/>a Church - Metropolitan Bible
     <br/>Baptist Church Toronto.
     <br/> I'm blessed and privileged to
@@ -42,7 +105,6 @@
 
 				<a class="buttons" href="about#content">Learn More</a>
 			</div>
-			
-			</div>
 		</div>
+	</div>
 	</div>
